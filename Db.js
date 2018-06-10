896020b202702cba7009559f93a21e1a77deaf1b
zenywallet <zenywallet@bitzeny.jp>
2018-06-11 00:02:44 +0900

    rocksdb test

---
+++ Db.js
@@ -0,0 +1,276 @@
+var rocksdb = require('level-packager')(require('rocksdb'));
+var fs = require('fs-extra');
+
+var prefix = {
+    blocks: 0x00,       // height = hash, time
+    txouts: 0x01,       // txid, n = value, [address, ..]
+    unspents: 0x02      // address, txid, n = value
+};
+
+var rocksdb_opts = {  
+    keyEncoding: 'binary',
+    valueEncoding: 'binary'
+};
+
+var uint8 = function(val) {
+    var v = new Buffer(1);
+    v.writeUInt8(val);
+    return v;
+}
+
+var uint32 = function(val) {
+    var v = new Buffer(4);
+    v.writeUInt32BE(val);
+    return v;
+}
+
+var uint64 = function(val) {
+    var v = new Buffer(8);
+    v.writeUInt32BE(Math.floor(val / 0x100000000), 0);
+    v.writeInt32BE(val & -1, 4);
+    return v;
+}
+
+var hex = function(val) {
+    return Buffer.from(val, 'hex');
+}
+
+var str = function(val) {
+    return Buffer.from(val, 'ascii');
+}
+
+var txid_min = hex('0000000000000000000000000000000000000000000000000000000000000000');
+var txid_max = hex('ffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff');
+var uint32_min = uint32(0);
+var uint32_max = uint32(0xffffffff);
+
+function Db(opts) {
+    var homepath = process.env.HOME || process.env.HOMEPATH || process.env.USERPROFILE;
+    var blockpath = homepath + '/.blockstor/blocks';
+    fs.mkdirsSync(blockpath);
+    var db = rocksdb(blockpath, rocksdb_opts);
+    var self = this;
+
+    this.put = function(key, val) {
+        return new Promise(function(resolve, reject) {
+            db.put(key, val, function(err) {
+                if(err) {
+                    reject(err);
+                    return;
+                }
+                resolve();
+            });
+        });
+    }
+
+    this.get = function(key, cb) {
+        return new Promise(function(resolve, reject) {
+            db.get(key, function(err, res) {
+                if(err) {
+                    if(err.name == 'NotFoundError') {
+                        resolve(null);
+                    } else {
+                        reject(err);
+                    }
+                    return;
+                }
+                if(cb) {
+                    resolve(cb(res));
+                } else {
+                    resolve(res);
+                }
+            });
+        });
+    }
+
+    this.del = function(key) {
+        return new Promise(function(resolve, reject) {
+            db.del(key, function(err) {
+                if(err) {
+                    reject(err);
+                    return;
+                }
+                resolve();
+            });
+        });
+    }
+
+    this.close = function() {
+        return new Promise(function(resolve, reject) {
+            db.close(function(err) {
+                if(err) {
+                    reject(err);
+                    return;
+                }
+                resolve();
+            });
+        });
+    }
+
+    this.setBlockHash = function(height, hash, time) {
+        var key = Buffer.concat([
+            uint8(prefix.blocks),
+            uint32(height)
+        ]);
+        var val = Buffer.concat([
+            hex(hash),
+            uint32(time)
+        ]);
+        return self.put(key, val);
+    }
+
+    this.getBlockHash = function(height) {
+        var key = Buffer.concat([
+            uint8(prefix.blocks),
+            uint32(height)
+        ]);
+        return self.get(key, function(res) {
+            return {hash: res.slice(0, 32).toString('hex'), time: res.readUInt32BE(32)};
+        });
+    }
+
+    this.delBlockHash = function(height) {
+        var key = Buffer.concat([
+            uint8(prefix.blocks),
+            uint32(height)
+        ]);
+        return self.del(key);
+    }
+
+    this.setTxout = function(txid, n, value, addresses) {
+        var key = Buffer.concat([
+            uint8(prefix.txouts),
+            hex(txid),
+            uint32(n)
+        ]);
+        var val = Buffer.concat([
+            uint64(value),
+            str(addresses.join(','))
+        ]);
+        return self.put(key, val);
+    }
+
+    this.getTxout = function(txid, n) {
+        var key = Buffer.concat([
+            uint8(prefix.txouts),
+            hex(txid),
+            uint32(n)
+        ]);
+        return self.get(key, function(res) {
+            return {value: res.readUInt32BE(0) * 0x100000000 + res.readUInt32BE(4), addresses: res.slice(8).toString().split(',')};
+        });
+    }
+
+    this.delTxout = function(txid, n) {
+        var key = Buffer.concat([
+            uint8(prefix.txouts),
+            hex(txid),
+            uint32(n)
+        ]);
+        return self.del(key);
+    }
+
+    this.setUnspent = function(address, txid, n, value) {
+        var key = Buffer.concat([
+            uint8(prefix.unspents),
+            str(address),
+            hex(txid),
+            uint32(n)
+        ]);
+        var val = Buffer.concat([
+            uint64(value)
+        ]);
+        return self.put(key, val);
+    }
+
+    this.getUnspent = function(address, txid, n) {  // address, [txid, n]
+        if(txid && n) {
+            var key = Buffer.concat([
+                uint8(prefix.unspents),
+                str(address),
+                hex(txid),
+                uint32(n)
+            ]);
+            return self.get(key, function(res) {
+                return res.readUInt32BE(0);
+            });
+        } else {
+            var p_unspent = uint8(prefix.unspents);
+            var str_addr = str(address);
+            var start = Buffer.concat([
+                p_unspent,
+                str_addr,
+                txid_min,
+                uint32_min
+            ]);
+            var end = Buffer.concat([
+                p_unspent,
+                str_addr,
+                txid_max,
+                uint32_max
+            ]);
+
+            var unspents = [];
+            return new Promise(function(resolve, reject) {
+                db.createReadStream({  
+                    gte: start,
+                    lte: end
+                }).on('data', function(res) {  
+                    var len = res.key.length;
+                    var key = res.key;
+                    unspents.push({
+                        txid: res.key.slice(len - 36, len - 4).toString('hex'),
+                        n: res.key.slice(len - 4, len + 1).readUInt32BE(0),
+                        value: res.value.readUInt32BE(0) * 0x100000000 + res.value.readUInt32BE(4)
+                    });
+                }).on('error', function(err) {
+                    reject(err);
+                }).on('close', function() {
+                    reject(null);
+                }).on('end', function() {
+                    resolve(unspents);
+                });
+            });
+        }
+    }
+
+    this.delUnspent = function(address, txid, n) {
+        var key = Buffer.concat([
+            uint8(prefix.unspents),
+            str(address),
+            hex(txid),
+            uint32(n)
+        ]);
+        return self.del(key);
+    }
+
+    this.getLastBlockHeight = function() {
+        return new Promise(function(resolve, reject) {
+            var start = Buffer.concat([
+                uint8(prefix.blocks),
+                uint32_min
+            ]);
+            var end = Buffer.concat([
+                uint8(prefix.blocks),
+                uint32_max
+            ]);
+            var last_height = null;
+            db.createReadStream({  
+                gte: start,
+                lte: end,
+                reverse: true,
+                limit: 1
+            }).on('data', function(res) {  
+                last_height = res.key.slice(1).readUInt32BE(0);
+            }).on('error', function(err) {
+                reject(err);
+            }).on('close', function() {
+                reject(null);
+            }).on('end', function() {
+                resolve(last_height);
+            });
+        });
+    }
+}
+
+module.exports = Db;
--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var opts = require('config');
 var Rpc = require('./Rpc');
 var bitcoin = require('bitcoinjs-lib');
-
+var Db = require('./Db');
 var rpc = new Rpc(opts);
 rpc.cb = function(cmd, err, res) {
     if(err) {
@@ -9,6 +9,7 @@ rpc.cb = function(cmd, err, res) {
     }
     return res;
 }
+var db = new Db(opts);
 
 bitcoin.networks['bitzeny'] = {
     messagePrefix: '\u0018Bitzeny Signed Message:\n',
@@ -24,59 +25,106 @@ bitcoin.networks['bitzeny'] = {
 var network = bitcoin.networks['bitzeny'];
 
 
+async function txins(tx, txid, txins_cb) {
+    await Promise.all(tx.ins.map(async function(input) {
+        var in_txid = input.hash.reverse().toString('hex');
+        var n = input.index;
+        if(n != 0xffffffff) {
+            var txout = await db.getTxout(in_txid, n);
+            if(!txout) {
+                throw('ERROR: Txout not found ' + in_txid + ' ' + n);
+            }
+
+            await txins_cb(in_txid, n, txout);
+        }
+    }));
+}
+
+async function txouts(tx, txid, txouts_cb) {
+    await Promise.all(tx.outs.map(async function(output, n) {
+        var amount = output.value;
+        var address = null;
+        try {
+            address = bitcoin.address.fromOutputScript(output.script, network);
+        } catch(ex) {}
+
+        if(address) {
+            await txouts_cb(txid, n, amount, [address]);
+        } else {
+            var addresses = [];
+            var chunks = bitcoin.script.decompile(output.script);
+            for(var k in chunks) {
+                var chunk = chunks[k];
+                if(Buffer.isBuffer(chunk) && chunk.length !== 1) {
+                    try {
+                        address = bitcoin.ECPair.fromPublicKeyBuffer(chunk, network).getAddress();
+                        addresses.push(address);
+                    } catch(ex) {}
+                }
+            }
+            if(addresses.length > 0) {
+                await txouts_cb(txid, n, amount, addresses);
+            } else {
+                address = '#' + txid + '-' + n;
+                await txouts_cb(txid, n, amount, [address]);
+            }
+        }
+    }));
+}
+
+async function txs_parser(block, txs_cb, txins_cb, txouts_cb) {
+    var txids = [];
+    await Promise.all(block.transactions.map(async function(tx, i) {
+        var txid = tx.getId();
+        txids[i] = txid;
+        await txs_cb(txid);
+        await txouts(tx, txid, txouts_cb);
+    }));
+
+    await Promise.all(block.transactions.map(async function(tx, i) {
+        await txins(tx, txids[i], txins_cb);
+    }));
+}
+
+
 ;(async function() {
     var blockcount = await rpc.getBlockCount();
     console.log(blockcount);
 
-    for(var i = blockcount - 9; i <= blockcount; i++) {
-        console.log('--- block #' + i);
-        var hash = await rpc.getBlockHash(i);
+    var last_height = await db.getLastBlockHeight() || 0;
+    console.log(last_height);
+
+    for(var height = last_height; height <= blockcount; height++) {
+        var hash = await rpc.getBlockHash(height);
         var rawblock = await rpc.getBlock(hash, false);
         var block = bitcoin.Block.fromHex(rawblock);
+        var time = block.timestamp;
 
-        block.transactions.map(function(tx, i) {
-            var txid = tx.getHash();
-            var txid_str = txid.reverse().toString('hex');
-            console.log('--- txid ' + txid_str);
+        await db.setBlockHash(height, hash, time);
 
-            tx.ins.map(function(input) {
-                var in_txid = input.hash;
-                var n = input.index;
-
-                if(n != 0xffffffff) {
-                    console.log('in: ' + in_txid.reverse().toString('hex') + '-' + n);
+        await txs_parser(block,
+            async function txs(txid) {
+            },
+            async function txins(txid, n, txout) {
+                for(var i in txout.addresses) {
+                    var address = txout.addresses[i];
+                    await db.delUnspent(address, txid, n);
                 }
-            });
+            },
+            async function txouts(txid, n, amount, addresses) {
+                await db.setTxout(txid, n, amount, addresses);
 
-            tx.outs.map(function(output, n) {
-                var amount = output.value;
-                var address = null;
-                try {
-                    address = bitcoin.address.fromOutputScript(output.script, network);
-                } catch(ex) {}
-
-                if(address) {
-                    console.log('out: ' + txid_str + '-' + n + ' ' + amount + ' ' + address);
-                } else {
-                    var addresses = [];
-                    var chunks = bitcoin.script.decompile(output.script);
-                    for(var k in chunks) {
-                        var chunk = chunks[k];
-                        if(Buffer.isBuffer(chunk) && chunk.length !== 1) {
-                            try {
-                                address = bitcoin.ECPair.fromPublicKeyBuffer(chunk, network).getAddress();
-                                addresses.push(address);
-                            } catch(ex) {}
-                        }
-                    }
-                    if(addresses.length > 0) {
-                        console.log('out: ' + txid_str + '-' + n + ' ' + amount + ' ' + addresses.join(','));
-                    } else {
-                        address = '#' + txid_str + '-' + n;
-                        console.log('out: ' + txid_str + '-' + n + ' ' + amount + ' ' + address);
-                    }
+                for(var i in addresses) {
+                    var address = addresses[i];
+                    await db.setUnspent(address, txid, n, amount);
                 }
-            });
-        });
+            }
+        );
+
+        if(height % 100 == 0) {
+            console.log(height);
+        }
     }
+
+    //console.log(await db.getUnspent('some address here!'));
 })();
