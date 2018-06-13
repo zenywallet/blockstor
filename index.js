f2370537c9fcdfb6d7c936a7c7b82d79119a1161
zenywallet <zenywallet@bitzeny.jp>
2018-06-13 22:37:30 +0900

    realtime block

--- index.js
+++ index.js
@@ -6,7 +6,14 @@ var ApiServer = require('./ApiServer');
 var rpc = new Rpc(opts);
 rpc.cb = function(cmd, err, res) {
     if(err) {
-        return null;
+        if(cmd == 'getBlockHash') {
+            if(err.code == -8 && err.message == 'Block height out of range') {
+                return null;
+            } else if(err.code == -1 && err.message == 'Block number out of range.') {
+                return null;
+            }
+        }
+        console.log(err);
     }
     return res;
 }
@@ -26,6 +33,61 @@ bitcoin.networks['bitzeny'] = {
 };
 var network = bitcoin.networks['bitzeny'];
 
+var aborting = false;
+async function abort() {
+    console.log("\rabort");
+    await db.close();
+    process.exit(1);
+    return new Promise(function() {});
+}
+
+process.on('unhandledRejection', console.dir);
+
+['SIGINT', 'SIGTERM'].forEach(function(evt) {
+    process.on(evt, async function() {
+        aborting = true;
+    });
+});
+
+var progress_flag = false;
+var progress_stop = false;
+var progress_t = null;
+function progress_agent() {
+    if(progress_stop) {
+        return;
+    }
+    progress_t = setTimeout(function() {
+        progress_flag = true;
+        progress_agent();
+    }, 1000);
+}
+
+function progress(msg) {
+    if(process.stdout.clearLine) {
+        process.stdout.clearLine();
+        process.stdout.write("\r" + msg);
+    } else {
+        console.log(msg);
+    }
+}
+
+function progress_enabled() {
+    progress_stop = false;
+    progress_agent();
+}
+
+function progress_disabled() {
+    clearTimeout(progress_t);
+    progress_flag = false;
+    progress_stop = true;
+    progress('');
+}
+
+function timestamp(time) {
+    var dt = time ? new Date(time * 1000) : new Date();
+    var dt_string = dt.toISOString().slice(0, 19).replace('T', ' ');
+    return dt_string;
+}
 
 async function txins(tx, txid, txins_cb) {
     await Promise.all(tx.ins.map(async function(input) {
@@ -88,45 +150,190 @@ async function txs_parser(block, txs_cb, txins_cb, txouts_cb) {
     }));
 }
 
+async function txs_rollback_parser(block, txs_cb, txins_cb, txouts_cb) {
+    var txids = [];
+    await Promise.all(block.transactions.map(async function(tx, i) {
+        var txid = tx.getId();
+        txids[i] = txid;
+        await txins(tx, txid, txins_cb);
+    }));
 
-;(async function() {
+    await Promise.all(block.transactions.map(async function(tx, i) {
+        var txid = txids[i];
+        await txouts(tx, txid, txouts_cb);
+        await txs_cb(txid);
+    }));
+}
+
+async function txs_parser_log(height, status, mode) {
     var blockcount = await rpc.getBlockCount();
-    console.log(blockcount);
+    if(blockcount != null) {
+        var msg = timestamp() + ' height(blockstor/coind)=' + height + '/' + blockcount
+            + ' delay=' + (blockcount - height) + (status ? ' - ' + status: '');
+        if(mode) {
+            progress(msg);
+        } else {
+            console.log(msg);
+        }
+    }
+}
 
-    var last_height = await db.getLastBlockHeight() || 0;
-    console.log(last_height);
+var height = 0;
+var prev_hash = null;
 
-    for(var height = last_height; height <= blockcount; height++) {
-        var hash = await rpc.getBlockHash(height);
-        var rawblock = await rpc.getBlock(hash, false);
-        var block = bitcoin.Block.fromHex(rawblock);
-        var time = block.timestamp;
+async function block_check() {
+    if(height > 0) {
+        var [hash, db_hash] = await Promise.all([rpc.getBlockHash(height), db.getBlockHash(height)]);
+        if(hash == null || db_hash == null) {
+            throw('ERROR: getBlockHash');
+        }
 
-        await db.setBlockHash(height, hash, time);
+        while(hash != db_hash.hash) {
+            console.log('rollback height=' + height);
 
-        await txs_parser(block,
-            async function txs(txid) {
-            },
-            async function txins(txid, n, txout) {
-                for(var i in txout.addresses) {
-                    var address = txout.addresses[i];
-                    await db.delUnspent(address, txid, n);
+            var rawblock = await rpc.getBlock(db_hash.hash, false);
+            if(!rawblock) {
+                throw('ERROR: getBlock');
+            }
+            var block = bitcoin.Block.fromHex(rawblock);
+
+            await txs_rollback_parser(block,
+                async function txs(txid) {
+                },
+                async function txins(txid, n, txout) {
+                    for(var i in txout.addresses) {
+                        var address = txout.addresses[i];
+                        await db.setUnspent(address, txid, n, txout.value);
+                    }
+                },
+                async function txouts(txid, n, amount, addresses) {
+                    for(var i in addresses) {
+                        var address = addresses[i];
+                        await db.delUnspent(address, txid, n);
+                    }
+
+                    await db.delTxout(txid, n);
                 }
-            },
-            async function txouts(txid, n, amount, addresses) {
-                await db.setTxout(txid, n, amount, addresses);
+            );
 
-                for(var i in addresses) {
-                    var address = addresses[i];
-                    await db.setUnspent(address, txid, n, amount);
+            await db.delBlockHash(height);
+
+            if(height > 0) {
+                height--;
+            } else {
+                break;
+            }
+
+            [hash, db_hash] = await Promise.all([rpc.getBlockHash(height), db.getBlockHash(height)]);
+            if(hash == null || db_hash == null) {
+                throw('ERROR: getBlockHash');
+            }
+        }
+    } else {
+        height = 0;
+        hash = await rpc.getBlockHash(height);
+        if(hash == null) {
+            throw('ERROR: getBlockHash');
+        }
+    }
+    prev_hash = height ? hash : null;
+}
+
+async function block_sync(suppress) {
+    var hash = await rpc.getBlockHash(height > 0 ? ++height : 0);
+
+    while(hash && !aborting) {
+        var rawblock = await rpc.getBlock(hash, false);
+        if(!rawblock) {
+            throw('ERROR: getBlock');
+        }
+
+        var block = bitcoin.Block.fromHex(rawblock);
+        var block_prevHash = block.prevHash.reverse().toString('hex');
+        if(prev_hash == block_prevHash || prev_hash == null) {
+            var time = block.timestamp;
+            await db.setBlockHash(height, hash, time);
+
+            function progress_out() {
+                if(suppress && progress_flag) {
+                    progress_flag = false;
+                    txs_parser_log(height, timestamp(time), true);
                 }
             }
-        );
 
-        if(height % 100 == 0) {
-            console.log(height);
+            await txs_parser(block,
+                async function txs(txid) {
+                },
+                async function txins(txid, n, txout) {
+                    for(var i in txout.addresses) {
+                        var address = txout.addresses[i];
+                        await db.delUnspent(address, txid, n);
+                    }
+                    progress_out();
+                },
+                async function txouts(txid, n, amount, addresses) {
+                    await db.setTxout(txid, n, amount, addresses);
+
+                    for(var i in addresses) {
+                        var address = addresses[i];
+                        await db.setUnspent(address, txid, n, amount);
+                    }
+                    progress_out();
+                }
+            );
+
+            if(!suppress) {
+                console.log(timestamp() + ' #' + height + ' ' + hash + ' ' + timestamp(time));
+            }
+            prev_hash = hash;
+        } else {
+            block_check();
+        }
+
+        hash = await rpc.getBlockHash(++height);
+    }
+
+
+    if(!hash && height > 0) {
+        height--;
+    }
+}
+
+;(async function() {
+    try {
+        height = await db.getLastBlockHeight() || 0;
+        await txs_parser_log(height, 'start');
+        await block_check();
+        progress_enabled();
+        await block_sync(true);
+        progress_disabled();
+        await txs_parser_log(height, aborting ? 'aborted' : 'synced');
+        if(aborting) {
+            await abort();
+        }
+    } catch(ex) {
+        console.log(ex);
+        await abort();
+    }
+
+    async function worker() {
+        try {
+            await block_check();
+            await block_sync();
+
+            if(aborting) {
+                await abort();
+            } else {
+                setTimeout(function() {
+                    worker();
+                }, 1000);
+            }
+        } catch(ex) {
+            console.log(ex);
+            await abort();
         }
     }
 
+    worker();
     apiserver.start();
 })();
